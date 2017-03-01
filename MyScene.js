09e68ce1c2ba8fee794025c39ea8762f48ff44aa
lubaochuan <lubaochuan@gmail.com>
2017-03-01 10:58:22 -0600

    add navigation and update index.android.js

--- MyScene.js
+++ MyScene.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Navigator } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { View, Text, StyleSheet, TouchableHighlight, Navigator } from 'react-native';
 
 export default class MyScene extends Component {
-  static get defaultProps() {
-    return {
-      title: 'MyScene'
-    };
-  }
-
   render() {
-    return (
-      <View style={styles.screen}>
-        <Text>Hi! My name is {this.props.title}.</Text>
-      </View>
-    )
+   return (
+     <View style={styles.screen}>
+       <Text>Current Scene: {this.props.title}</Text>
+
+       <TouchableHighlight onPress={this.props.onForward}>
+         <Text>Tap me to load the next scene</Text>
+       </TouchableHighlight>
+
+       <TouchableHighlight onPress={this.props.onBack}>
+         <Text>Tap me to go back</Text>
+       </TouchableHighlight>
+     </View>
+   )
   }
 }
 
@@ -22,3 +24,9 @@ const styles = StyleSheet.create({
     paddingTop: 30
   }
 });
+
+MyScene.propTypes = {
+  title: PropTypes.string.isRequired,
+  onForward: PropTypes.func.isRequired,
+  onBack: PropTypes.func.isRequired,
+};
--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Navigator } from 'react-native';
 
 import MyScene from './MyScene';
 
 class MyApp extends Component {
   render() {
     return (
-      <MyScene />
+      <Navigator
+        initialRoute={{ title: 'My Initial Scene', index: 0 }}
+        renderScene={(route, navigator) =>
+          <MyScene
+            title={route.title}
+            // Function to call when a new scene should be displayed
+            onForward={() => {
+              const nextIndex = route.index + 1;
+              navigator.push({
+                title: 'Scene ' + nextIndex,
+                index: nextIndex,
+              });
+            }}
+            // Function to call to go back to the previous scene
+            onBack={() => {
+              if (route.index > 0) {
+                navigator.pop();
+              }
+            }}
+          />
+        }
+      />
     )
   }
 }
